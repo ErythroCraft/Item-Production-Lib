28f69a6e90593089e106dfbacc7d496272a0c841
Hinterix <motono@outlook.de>
2026-06-25 10:05:19 +0200

    U_020

--- 1.20.1/src/main/java/daripher/itemproduction/mixin/minecraft/CampfireBlockEntityMixin.java
+++ 1.20.1/src/main/java/daripher/itemproduction/mixin/minecraft/CampfireBlockEntityMixin.java
@@ -1,29 +1,29 @@
 package daripher.itemproduction.mixin.minecraft;
 
 import daripher.itemproduction.ItemProductionLib;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.Container;
+import net.minecraft.world.Containers;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.CampfireBlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CampfireBlockEntity.class)
 public class CampfireBlockEntityMixin {
 
-    @Inject(method = "cookTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Containers;dropItemStack(Lnet/minecraft/world/Level;DDDLnet/minecraft/world/item/ItemStack;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void modifyCampfireOutput(
-            Level level, BlockPos pos, BlockState state, CampfireBlockEntity blockEntity,
-            CallbackInfo ci, int i, Container container, ItemStack itemStack) {
-        if (itemStack != null && !itemStack.isEmpty()) {
-            ItemStack modified = ItemProductionLib.itemProduced(itemStack.copy(), blockEntity);
-            itemStack.setTag(modified.getTag());
-            itemStack.setCount(modified.getCount());
-        }
+    private CampfireBlockEntityMixin() {
+        throw new IllegalStateException("Mixin class cannot be instantiated");
+    }
+
+    @Redirect(method = "cookTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Containers;dropItemStack(Lnet/minecraft/world/Level;DDDLnet/minecraft/world/item/ItemStack;)V"))
+    private static void modifyCampfireOutput(Level level, double x, double y, double z, ItemStack stack) {
+
+        net.minecraft.world.level.block.entity.BlockEntity blockEntity = level
+                .getBlockEntity(new net.minecraft.core.BlockPos((int) x, (int) y, (int) z));
+
+        ItemStack modifiedStack = ItemProductionLib.itemProduced(stack.copy(), blockEntity);
+
+        Containers.dropItemStack(level, x, y, z, modifiedStack);
     }
 }
